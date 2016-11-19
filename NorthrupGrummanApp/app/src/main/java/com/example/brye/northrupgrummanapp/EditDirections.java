4ac20d6b10ece67cbe259572a3ba0b841322c999
Brian Vo <ovnairb@gmail.com>
2016-11-18 19:56:44 -0800

    Second Activity works

---
+++ NorthrupGrummanApp/app/src/main/java/com/example/brye/northrupgrummanapp/EditDirections.java
@@ -0,0 +1,13 @@
+package com.example.brye.northrupgrummanapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditDirections extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_directions);
+    }
+}
--- NorthrupGrummanApp/app/src/main/java/com/example/brye/northrupgrummanapp/HomePage.java
+++ NorthrupGrummanApp/app/src/main/java/com/example/brye/northrupgrummanapp/HomePage.java
@@ -1,8 +1,10 @@
 package com.example.brye.northrupgrummanapp;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.widget.Button;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -11,21 +13,25 @@ import android.view.MenuItem;
 
 public class HomePage extends AppCompatActivity {
 
+    public Button button;
+
+    public void init() {
+        button = (Button) findViewById(R.id.btn_newDirection);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, EditDirections.class);
+                startActivity(intent);
+            }
+        });
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        init();
     }
 
     @Override
@@ -41,12 +47,11 @@ public class HomePage extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
 }
