d10dd361897344f1d196be59e632a87f2caeece1
Alex Jones <alex.theboy.jones@gmail.com>
2016-11-19 03:02:17 -0800

    mergin for day

--- NorthrupGrummanApp/app/src/main/java/com/example/brye/northrupgrummanapp/HomePage.java
+++ NorthrupGrummanApp/app/src/main/java/com/example/brye/northrupgrummanapp/HomePage.java
@@ -1,8 +1,18 @@
 package com.example.brye.northrupgrummanapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -11,13 +21,16 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class HomePage extends AppCompatActivity {
+public class HomePage extends AppCompatActivity implements LocationListener {
 
     public Button button;
+    public TextView loc;
+    public LocationManager locationManager;
 
     //Initialize the Edit Directions button
     public void init() {
         button = (Button) findViewById(R.id.btn_newDirection);
+        loc = (TextView) findViewById(R.id.loc);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(HomePage.this, EditDirections.class);
@@ -33,7 +46,18 @@ public class HomePage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         init();
+<<<<<<< Updated upstream
         Notify.newNotification("Update Alert", "Traffic: Severe Accident", this);
+=======
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if ( ContextCompat.checkSelfPermission( this, android.Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            ActivityCompat.requestPermissions( this, new String[] {  android.Manifest.permission.ACCESS_COARSE_LOCATION  },1);
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 0, this);
+
+>>>>>>> Stashed changes
     }
 
     @Override
@@ -49,11 +73,33 @@ public class HomePage extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        Notify.newNotification("ayy","lmao",this);
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        loc.setText("Latitude:" + location.getLatitude() + ", Longitude:" + location.getLongitude());
+        Notify.newNotification( Double.toString(location.getLatitude()),Double.toString(location.getLongitude()),this);
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude","disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude","status");
     }
 
 }
